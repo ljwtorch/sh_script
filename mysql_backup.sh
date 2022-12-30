09c15bd1a403c4340134cbfe159b276610971c73
torch <ljwtorch@gmail.com>
2022-12-30 15:40:34 +0800

    添加备份命令

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -14,7 +14,7 @@ backup_days=7
 backup_time='date +%Y%m%d%H%M'
 
 #docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > /home/backup-$mysql_database-$backup_time.sql
-docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > ${backup_dir}/backup-${mysql_database}-${backup_time}.sql
+docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > "${backup_dir}"/backup-"${mysql_database}"-"${backup_time}".sql
 # 拷贝到主机内
 #docker cp $container_name:/home/backup-$mysql_database-$backup_time.sql $backup_dir
 # 删除容器内备份
