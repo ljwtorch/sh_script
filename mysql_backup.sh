bd8c18a9896098ed3000b681d3b90d7b3bb9851f
torch <ljwtorch@gmail.com>
2022-12-30 15:04:20 +0800

    添加备份命令

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -13,4 +13,6 @@ backup_old_delete="true"
 backup_days=7
 backup_time='date +%Y%m%d%H%M'
 
-docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > $backup_dir/backup-$mysql_database-$backup_time.sql
+docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > /home/backup-$mysql_database-$backup_time.sql
+docker cp $container_name:/home/backup-$mysql_database-$backup_time.sql $backup_dir
+docker exec mysql rm /home/backup-$mysql_database-$backup_time.sql
