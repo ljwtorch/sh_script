6302157825ae78d000afdcdf616dcb416f2dacda
torch <ljwtorch@gmail.com>
2022-12-30 16:30:40 +0800

    添加备份命令

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -11,7 +11,7 @@ backup_dir="/home/torch/backup_all/mysql_backup"
 # 是否删除旧数据
 backup_old_delete="true"
 backup_days=7
-backup_time='date +%Y%m%d%H%M'
+backup_time="date +%Y%m%d%H%M"
 
 #docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > /home/backup-$mysql_database-$backup_time.sql
 docker exec $container_name mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $mysql_database > "$backup_dir"/"backup-$mysql_database-$backup_time".sql
