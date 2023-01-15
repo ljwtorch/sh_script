28695bf41862fde8d942ef540127aa304b2e7437
torch <ljwtorch@gmail.com>
2023-01-15 09:46:44 +0800

    修改清空方法

--- koko_backup.sh
+++ koko_backup.sh
@@ -3,9 +3,9 @@ backup_time=`date +%Y%m%d%H%M`
 backup_dir="/home/torch/log/koko_backup"
 
 #执行koko日志脚本
-cd /home/torch/go_code/koko_log && go run main.go
+cd /home/torch/go_code/koko_log && /usr/local/go/bin/go run main.go
 
-#备份koko日志并删除当前目录的koko.log (需要事先在当前目录创建koko_backup文件夹)
+#备份koko日志并清空当前目录的koko.log防止重复内容推送 (需要事先在当前目录创建koko_backup文件夹)
 sudo cp /home/torch/log/koko.log "$backup_dir"/koko-$backup_time.log
-echo "模拟删除"
-sudo rm /home/torch/log/koko.log
+
+sudo echo "" > /home/torch/log/koko.log
