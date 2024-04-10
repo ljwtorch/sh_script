07147b026cbc4e4a06e8772c9b530f6b707f8d69
lijuwei <ju_wei.li@ceasia.cn>
2024-04-10 10:15:55 +0800

    update

--- rsync.sh
+++ rsync.sh
@@ -4,8 +4,8 @@ set -o errexit	#发生错误时立即退出shell
 set -o nounset	#尝试使用未设置的变量时引发错误
 set -o pipefail	#管道命令中，任一命令失败则整个管道命令标记为失败
 
-readonly QL_DIR="/home/torch/software/qinglong"
-readonly MYSQL_DIR="/home/torch/software/mysql"
+#readonly QL_DIR="/home/torch/software/qinglong"
+#readonly MYSQL_DIR="/home/torch/software/mysql"
 readonly SOFTWARE_DIR="/home/torch/software"
 readonly BACKUP_PATH="/mnt/king/rsync/"
 
